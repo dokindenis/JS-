487212a5e7bd3eeb4717e98088728849bee7accf
Denis <thinthinlean@gmail.com>
2021-02-21 20:28:53 +0300

    Первое задание

--- js/main.js
+++ js/main.js
@@ -5,21 +5,17 @@ const products = [
     {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-}
+const renderProduct = ( { title, price}, img='https://placehold.it/200x150') => `<div class="product-item" data-id="${this.id}">
+                <img src="${img}" alt="product image">
+                <div class="desc">
+                    <h3>${title}</h3>
+                    <p>${price}</p>
+                    <button class="by-btn">Добавить в корзину</button>
+            </div>
+        </div>`;
 
-const renderProducts = (list) => {
-    const productList = list.map((item) => {
-        return renderProduct(item.title, item.price);
-    });
-
-    console.log(productList);
-    document.querySelector('.products').innerHTML = productList;
-}
+const renderProducts = list => {
+    document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
+};
 
 renderProducts(products);
